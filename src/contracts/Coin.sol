870c1e4a42a458d2b5c81ac323a986987f0a48ca
Rannie Ollit <einnar82@gmail.com>
2021-10-01 18:10:54 +0800

    WIP

--- src/contracts/Coin.sol
+++ src/contracts/Coin.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Coin is ERC20 {
     constructor(uint256 initialSupply) ERC20("Token", "TKN") {
-      super._mint(msg.sender, SafeMath.mul(initialSupply, 10 ** decimals()));
+        _mint(msg.sender, initialSupply * 10 ** decimals());
     }
 
     /**
@@ -19,7 +18,7 @@ contract Coin is ERC20 {
      * - the caller must have the `MINTER_ROLE`.
      */
     function mint(address to, uint256 amount) public virtual {
-        super._mint(to, SafeMath.mul(amount, 10 ** decimals()));
+        _mint(to, amount * 10 ** decimals());
         // super._mint(to, amount);
     }
 
@@ -34,7 +33,7 @@ contract Coin is ERC20 {
         returns (uint256)
     {
         uint256 currentBalance_ = super.balanceOf(account);
-        return SafeMath.div(currentBalance_, 10 ** decimals());
+        return currentBalance_ / (10**decimals());
     }
 
     /**
@@ -42,32 +41,6 @@ contract Coin is ERC20 {
      */
     function totalSupply() public view virtual override returns (uint256) {
         uint256 totalSupply_ = super.totalSupply();
-        return SafeMath.div(totalSupply_, 10 ** decimals());
-    }
-
-    /**
-     * @dev Hook that is called before any transfer of tokens. This includes
-     * minting and burning.
-     *
-     * Calling conditions:
-     *
-     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-     * will be transferred to `to`.
-     * - when `from` is zero, `amount` tokens will be minted for `to`.
-     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override {
-        super._beforeTokenTransfer(
-            from,
-            to,
-            SafeMath.mul(amount, 10 ** decimals())
-        );
+        return totalSupply_ / (10**decimals());
     }
 }
