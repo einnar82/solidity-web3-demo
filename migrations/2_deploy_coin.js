c703197672e0c5e4397aefd5bb17b0f02063763c
Rannie Ollit <einnar82@gmail.com>
2021-10-12 10:16:12 +0800

    Change migration

--- migrations/2_deploy_coin.js
+++ migrations/2_deploy_coin.js
@@ -1,9 +1,10 @@
-const { web3 } = require("../src/config");
+const Web3 = require('web3');
 
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 const Coin = artifacts.require("Coin");
 
 module.exports = function (deployer) {
-  const initialAmount = 10;
+  const initialAmount = '10';
   const safeAmount = web3.utils.toWei(initialAmount, 'ether')
   const tokens = web3.utils.toBN(safeAmount)
   deployer.deploy(Coin, tokens);
