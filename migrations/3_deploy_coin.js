581a46247c157e5a866496dcd5a3ef5e7fbe802e
Rannie Ollit <einnar82@gmail.com>
2021-10-12 09:35:33 +0800

    CHange migration

--- migrations/3_deploy_coin.js
+++ migrations/3_deploy_coin.js
@@ -1,7 +1,10 @@
+const { web3 } = require("../src/config");
+
 const Coin = artifacts.require("Coin");
 
 module.exports = function (deployer) {
   const initialAmount = 10;
-  const amountInWei = initialAmount * Math.pow(10, 18);
-  deployer.deploy(Coin, String(amountInWei));
+  const safeAmount = web3.utils.toWei(initialAmount, 'ether')
+  const tokens = web3.utils.toBN(safeAmount)
+  deployer.deploy(Coin, tokens);
 };
