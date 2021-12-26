bceb6d7853382f3c88f9d7af424a551145ef9961
Rannie Ollit <rannie@mandnweb.com>
2021-12-26 11:03:13 +0800

    change server listen

--- src/index.js
+++ src/index.js
@@ -1,40 +1,46 @@
-import express from 'express'
+import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import * as dotenv from "dotenv";
-import coinRoute from './routes/coin.route'
+import * as http from "http";
+import coinRoute from "./routes/coin.route";
 
 dotenv.config();
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 4002;
 
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(helmet());
 app.use(cors());
 
-app.use('/coins', coinRoute);
-app.get('/', (req, res) => {
-    res.json({
-        message: 'ok'
-    })
-})
+app.use("/coins", coinRoute);
+app.get("/", (req, res) => {
+  res.json({
+    message: "ok",
+  });
+});
 
 app.use((req, res, next) => {
-    return res.status(404).json({
-        error: 'Not found'
-    })
+  return res.status(404).json({
+    error: "Not found",
+  });
 });
 
 app.use((err, req, res, next) => {
-    return res.status(err.status || 500).json({
-        error: err
-    });
+  return res.status(err.status || 500).json({
+    error: err,
+  });
 });
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
+
